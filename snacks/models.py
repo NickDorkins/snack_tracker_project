bf70e79bb164ec544f76bb7eff200fdf08524381
NickDorkins <nick.dorkins@gmail.com>
2021-03-04 21:10:40 -0800

    first commit

---
+++ snacks/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth import get_user_model
+
+# (models.Model)
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name 
